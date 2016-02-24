3908c3ca8f476cf8ce70636f743a34bc95d9a87c
badim <goloveychuk@mail.ru>
2016-02-24 17:06:04 +0200

    init

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftOracle",
     dependencies: [
-        .Package(url: "../cocilib", majorVersion: 0),
+        .Package(url: "https://github.com/goloveychuk/cocilib.git", majorVersion: 0),
         .Package(url: "https://github.com/Zewo/SQL.git", majorVersion: 0, minor: 2),
     ]
 )
