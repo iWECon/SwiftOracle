84a39515060db5e6cfa1943d9cecbaaff13d3cea
goloveychuk <goloveychuk@users.noreply.github.com>
2016-03-19 01:02:27 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,6 @@ let package = Package(
     name: "SwiftOracle",
     dependencies: [
         .Package(url: "https://github.com/goloveychuk/cocilib.git", majorVersion: 0),
-        .Package(url: "https://github.com/Zewo/Data.git", majorVersion: 0, minor: 2)
+        .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
     ]
 )
