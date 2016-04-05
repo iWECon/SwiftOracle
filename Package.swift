385cec58eddf88fa5f09b8ffec91be3d8d65a8e7
badim <goloveychuk@mail.ru>
2016-04-05 10:48:10 +0300

    move to new spm

--- Package.swift
+++ Package.swift
@@ -7,8 +7,8 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftOracle",
+    targets: [Target(name: "SwiftOracle", dependencies: ["cocilib"])],
     dependencies: [
-        .Package(url: "https://github.com/goloveychuk/cocilib.git", majorVersion: 0),
         .Package(url: "https://github.com/Zewo/String.git", majorVersion: 0, minor: 4),
     ]
 )
