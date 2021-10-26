a4fabd283af90cfe493e213418ae9ec934c62aa6
iWw <i.1214@yahoo.com>
2021-10-26 14:41:52 +0800

    update

--- Package.swift
+++ Package.swift
@@ -1,14 +1,17 @@
-
-
-
-
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftOracle",
-    targets: [Target(name: "SwiftOracle", dependencies: ["cocilib"])],
+    products: [
+        .library(name: "SwiftOracle", targets: ["SwiftOracle"])
+    ],
     dependencies: [
-        
+        .package(url: "https://github.com/iliasaz/cocilib", .upToNextMajor(from: "1.1.0"))
+    ],
+    targets: [
+        .target(name: "SwiftOracle", dependencies: ["cocilib"])
     ]
 )
